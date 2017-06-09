b9a918a209b0ff6d56c692e3fbcf5a31df47687d
forsythe <sxthe@protonmail.ch>
2017-06-09 09:41:04 -0500

    changes

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -17,19 +17,27 @@ client.on('message', message => {
 // swear filter
 client.on('message', message => {
     if (message.content.includes('swear')) {
-        // message.delete();
-        message.edit('i just used a bad word')
+        tempmsg = message
+        message.delete();
+        tempmsg.reply("http://imgh.us/swe.jpg")
     }
 });
 
-// spicy
+// how do dice even work
+function dice(sides) {
+    return Math.floor((Math.random() * sides) + 1); 
+}
+
+// roll a di(c)e 
+// usage: .dN
+// where N is the number of sides
 client.on('message', message => {
-    if (message.content.includes('spicy')) {
-        message.react('hot_pepper');
-    }
+    if (message.content.slice(0,2) === '.d' && !isNaN(message.content.slice(2,-1))) {
+        message.channel.send(dice(message.content.slice(2,message.content.length)));                                                      
+    }   
 });
 
 // log in to the server
 console.log('logging in...');
 client.login('MjY0ODg0OTc0MTgyODU4NzUz.DBuDRA.6u2vT-lREt19v6EBXq8G1usC2BY');
-console.log('Logged in!');
+console.log('logged in!');
